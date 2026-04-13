d3b73d40665d8b2ef3822f8ef4803aaf2d089880
DonBnellHubMove <donbracknell@gmail.com>
2026-04-13 09:54:17 +0100

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+FILENAME: src/index.js
+LOCATION: src/ folder
+CONTENT:
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
